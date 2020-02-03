a064855296461084d4a165dcd84ac00095f7c76c
Segey Naumov <tweet9ra@mail.ru>
2020-02-03 16:31:12 +0300

    Auth command test and phpunit config

---
+++ tests/Acceptance/AuthCommandTest.php
@@ -0,0 +1,71 @@
+<?php
+
+
+namespace Tests\Unit;
+
+
+use PHPUnit\Framework\TestCase;
+use tweet9ra\Logux\App;
+
+class AuthCommandTest extends TestCase
+{
+    protected $successfulAuthCommand = ["auth", "38", "good-token", "gf4Ygi6grYZYDH5Z2BsoR"];
+    protected $noTokenProvidedExceptionMessage = 'Expected exception error message when token has bad format';
+
+    /**
+     * @dataProvider dataProvider
+     * @param array $authCommand
+     * @param array $expectedResponse
+     */
+    public function testProcessCorrectAuthCommand(array $authCommand, array $expectedResponse)
+    {
+        $app = App::getInstance();
+        $app->setActionsMap([
+            'auth' => function (string $authId, string $userId = null, string $token = null) {
+                // Allowing access to all guests
+                if (!$userId) {
+                    return true;
+                }
+
+                // Throwing error
+                if (!$token) {
+                    throw new \Exception($this->noTokenProvidedExceptionMessage);
+                }
+
+                $successfulAuthCommand = $this->successfulAuthCommand;
+                return $authId == $successfulAuthCommand[3]
+                    && $userId == $successfulAuthCommand[1]
+                    && $token == $successfulAuthCommand[2];
+            }
+        ]);
+
+        $successResponse = $app->processCommands([$authCommand]);
+        $this->assertSame([$expectedResponse], $successResponse);
+    }
+
+    public function dataProvider()
+    {
+        return [
+            // Successful user authentication
+            [
+                $this->successfulAuthCommand,
+                ['authenticated', 'gf4Ygi6grYZYDH5Z2BsoR']
+            ],
+            // Successful guest authentication
+            [
+                ["auth", false, null, "gf4Ygi6grYZYDH5Z2BsoR"],
+                ['authenticated', 'gf4Ygi6grYZYDH5Z2BsoR']
+            ],
+            // Failed atuhentication caused by bad token
+            [
+                ["auth", "38", "bad-token", "gf4Ygi6grYZYDH5Z2BsoR"],
+                ['denied', 'gf4Ygi6grYZYDH5Z2BsoR']
+            ],
+            // Authentication failed with error message
+            [
+                ["auth", "38", false, "gf4Ygi6grYZYDH5Z2BsoR"],
+                ['error', $this->noTokenProvidedExceptionMessage]
+            ],
+        ];
+    }
+}
